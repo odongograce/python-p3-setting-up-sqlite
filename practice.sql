90dc4be6609c3447d7f1562c630af3251fdab810
Grace Odongo <graceakinyi024@gmail.com>
2025-11-26 08:10:34 +0300

    new

---
+++ practice.sql
@@ -0,0 +1,23 @@
+-- CRUD operations
+-- 1. Create
+-- CREATE TABLE students(
+--     id  INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER
+--     );
+-- ALTER TABLE students ADD COLUMN course TEXT;
+-- INSERT INTO students(name, age, course) VALUES ("Natalie" , 16 , "Computer Science");
+-- INSERT INTO students(name, age, course) VALUES ("John" , 22 , "Nursing");
+-- INSERT INTO students(name, age, course) VALUES ("Fortune" , 10 , "Commerce");
+
+-- 2. Retrieve
+-- SELECT * FROM students;
+-- SELECT name, age FROM students;
+-- SELECT id, name FROM students;
+-- SELECT DISTINCT name FROM students;
+
+-- 3. Update
+
+-- 4. Delete
+-- DROP TABLE students
+
